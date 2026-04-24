8d34ac3e7ffe6c72ece2a80d544a510af127da16
Navya Sri Gannavarapu <navyagannavarapu9@gmail.com>
2026-04-24 22:20:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,12 +45,20 @@ capital_loss = st.sidebar.slider("Capital Loss", 0, 4356, 0)
 native_country = st.sidebar.selectbox("Native Country", [
     "United-States", "mexcio", "Germany"])
 # Build input DataFrame (⚠️ must match preprocessing of your training data)
-input_df = input_df([
-    'age', 'workclass', 'fnlwgt', 'educational-num', 'marital-status',
-       'occupation', 'relationship', 'race', 'gender', 'capital-gain',
-       'capital-loss', 'hours-per-week', 'native-country'],
-      dtype='object')
+input_df = pd.DataFrame({
+    'age': [age],
+    'workclass': [workclass],
+    'fnlwgt': [fnlwgt],
+    'educational-num': [educational_num],
+    'marital-status': [marital_status],
+    'occupation': [occupation],
+    'relationship': [relationship],
+    'race': [race],
+    'gender': [gender]
+})
 
+# Ensure correct column order
+input_df = input_df[['age','workclass','fnlwgt','educational-num','marital-status','occupation','relationship','race','gender']]
 from sklearn.preprocessing import LabelEncoder
 
 encoder = LabelEncoder()
